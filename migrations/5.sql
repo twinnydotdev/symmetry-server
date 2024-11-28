df9882e9355634bc8e082be07b2de64abb6e63a7
rjmacarthy <richardmacarthy@hotmail.com>
2024-11-28 09:56:23 +0000

    fix

--- migrations/5.sql
+++ migrations/5.sql
@@ -1,5 +1,14 @@
 BEGIN TRANSACTION;
 
+UPDATE provider_sessions 
+SET end_time = CURRENT_TIMESTAMP 
+WHERE id NOT IN (
+    SELECT MAX(id) 
+    FROM provider_sessions 
+    WHERE end_time IS NULL 
+    GROUP BY peer_key
+);
+
 CREATE UNIQUE INDEX idx_active_sessions ON provider_sessions(peer_key) 
 WHERE end_time IS NULL;
 
