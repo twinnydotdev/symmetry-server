0c0c65feb7af715f359118ce20626a65b15c79dc
rjmacarthy <richardmacarthy@hotmail.com>
2025-01-26 21:21:22 +0000

    lower end

--- src/database.ts
+++ src/database.ts
@@ -5,12 +5,13 @@ const dbPath = path.join(__dirname, "../sqlite.db");
 
 export const database = new sqlite3.Database(dbPath, (err) => {
   if (err) {
-    console.error("Error opening database:", err.message);
+    console.error('Error opening database:', err.message);
   } else {
-    database.run("PRAGMA journal_mode = WAL;");
-    database.run("PRAGMA synchronous = NORMAL;");
-    database.run("PRAGMA cache_size = -64000;");
-    database.run("PRAGMA temp_store = MEMORY;");
-    database.run("PRAGMA mmap_size = 30000000000;");
+    database.run('PRAGMA journal_mode = WAL;');
+    database.run('PRAGMA synchronous = NORMAL;');
+    database.run('PRAGMA cache_size = -16000;'); // 16MB cache
+    database.run('PRAGMA temp_store = MEMORY;');
+    database.run('PRAGMA mmap_size = 268435456;'); // 256MB mmap
+    database.run('PRAGMA page_size = 4096;');
   }
 });
